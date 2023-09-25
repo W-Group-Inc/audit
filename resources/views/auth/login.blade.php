ca01bb06692d4a29a6e81d3fed778d4e4f98faad
renzcabato1 <cabato.renz.renz@gmail.com>
2023-09-25 09:52:38 +0800

    Done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,121 +1,110 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<section class="ftco-section">
+{{-- <section class="ftco-section">
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-6 text-center mb-5">
-                <h2 class="heading-section">Login #10</h2>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-md-6 col-lg-4">
+        <div class="row justify-content-center ">
+            <div class="col-md-6 col-lg-4 rounded" style='background: rgba(0, 0, 0, .5)'>
                 <div class="login-wrap p-0">
-              <h3 class="mb-4 text-center">Have an account?</h3>
-              <form action="#" class="signin-form">
-            <div class="login100-form-avatar">
-                <img src="images/avatar-01.jpg" alt="AVATAR">
+                    
+            <div class="login100-form-avatar text-center" >
+                <img src="{{asset('login_css/images/icon.png')}}" alt="AVATAR" style='width:350px;'>
+            </div>
+              <h3 class="mb-4 text-center ">AUDIT PORTAL BPD</h3>
+              
+              <form method="POST" action="{{ route('login') }}">
+                @csrf
+            <div class="form-group ">
+                <input id="email" type="email" class="form-control form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="Email" name="email" value="{{ old('email') }}" required autofocus>
+
             </div>
             <div class="form-group">
-                <input type="text" class="form-control" placeholder="Username" required>
-            </div>
-            <div class="form-group">
-              <input id="password-field" type="password" class="form-control" placeholder="Password" required>
+                <input id="password-field" type="password" class="form-control form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="Password" required>
+
+               
               <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
             </div>
+            
+            @if ($errors->has('email'))
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+            @endif
+            @if ($errors->has('password'))
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $errors->first('password') }}</strong>
+                </span>
+            @endif
             <div class="form-group">
                 <button type="submit" class="form-control btn btn-primary submit px-3">Sign In</button>
             </div>
             <div class="form-group d-md-flex">
                 <div class="w-50">
-                    <label class="checkbox-wrap checkbox-primary">Remember Me
-                                  <input type="checkbox" checked>
-                                  <span class="checkmark"></span>
-                                </label>
-                            </div>
-                            <div class="w-50 text-md-right">
-                                <a href="#" style="color: #fff">Forgot Password</a>
-                            </div>
+                </div>
+                <div class="w-50 text-md-right">
+                    <a href="#" style="color: #fff">Forgot Password</a>
+                </div>
             </div>
           </form>
-          <p class="w-100 text-center">&mdash; Or Sign In With &mdash;</p>
-          <div class="social d-flex text-center">
-              <a href="#" class="px-2 py-2 mr-md-1 rounded"><span class="ion-logo-facebook mr-2"></span> Facebook</a>
-              <a href="#" class="px-2 py-2 ml-md-1 rounded"><span class="ion-logo-twitter mr-2"></span> Twitter</a>
-          </div>
+          
           </div>
             </div>
         </div>
     </div>
-</section>
+</section> --}}
+<section class="ftco-section">
+    <div class="container">
+        @if($errors->any())
+        <div class="row justify-content-center">
+            <div class="col-md-12 col-lg-10">
+                <div class="form-group alert alert-danger alert-dismissable">
+                    <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
+                    <strong>{{$errors->first()}}</strong>
+                </div>
+            </div>
+        </div>
+    @endif
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+        <div class="row justify-content-center">
+            <div class="col-md-12 col-lg-10">
+                <div class="wrap d-md-flex">
+                    <div class="img" style="background-image: url(login_css/images/APS.png);">
+              </div>
+                    <div class="login-wrap p-4 p-md-5">
+                  <div class="d-flex">
+                      <div class="w-100">
+                          <h3 class="mb-4">Sign In</h3>
+                      </div>
+                  </div>
+                        <form action="#" class="signin-form">
+                      <div class="form-group mb-3">
+                          <label class="label" for="name">Email</label>
+                         <input id="email" type="email" class="form-control " placeholder="Email" name="email" value="{{ old('email') }}" required autofocus>
 
+                      </div>
+                <div class="form-group mb-3">
+                    <label class="label" for="password">Password</label>
+                    <input id="password-field" type="password" class="form-control" name="password" placeholder="Password" required>
+                </div>
+              
+                <div class="form-group">
+                    <button type="submit" class="form-control btn btn-primary rounded submit px-3">Sign In</button>
+                </div>
+                <div class="form-group d-md-flex">
+                    <div class="w-50 text-left">
+                    </div>
+                    <div class="w-50 text-md-right">
+                        <a href="{{ route('password.request') }}">Forgot Password</a>
+                    </div>
+                </div>
+              </form>
+            </div>
+          </div>
+            </div>
+        </div>
+        </form>
+    </div>
+</section>
 @endsection
